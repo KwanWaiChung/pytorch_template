918366808e9dbbe18c38703fe2bccd4a5a58e448
student-t <student-t@student-ts-MacBook-Pro.local>
2019-07-25 17:22:52 +0800

    In the middle of changing pipeline to use China of responsibility pattern

--- data/pipeline.py
+++ data/pipeline.py
@@ -2,34 +2,29 @@ from typing import Callable, List, Union
 
 
 class Pipeline:
-    def __init__(self, func: Callable[List[str], List[str]]):
-        if func is None:
+    def __init__(self, name: str, func: Callable[[str], str]):
+        if func:
             self.func = func
         else:
             self.func = Pipeline.identity
-        self.pipes[self]
+        self.next = None
+        self.name = name
 
     def __call__(self, sentence: List[str]):
-        for pipe in self.pipes:
-            sentence = pipe(sentence)
-        return sentence
+        x = self.func(sentence)
+        if self.next:
+            return self.next(x)
+        return x
 
-    def add_before(
-        self, pipeline: Union["Pipeline", Callable[List[str], List[str]]]
+    def add(
+        self, pipeline: Union["Pipeline", Callable[[str], str]]
     ) -> "Pipeline":
         if not isinstance(pipeline, Pipeline):
             pipeline = Pipeline(pipeline)
-        self.pipes = pipeline.pipes + self.pipes
-        return self
-
-    def add_after(
-        self, pipeline: Union["Pipeline", Callable[List[str], List[str]]]
-    ) -> "Pipeline":
-        if not isinstance(pipeline, Pipeline):
-            pipeline = Pipeline(pipeline)
-        self.pipes = self.pipes + pipeline.pipes
+        pipeline.next = self.next
+        self.next = pipeline
         return self
 
     @staticmethod
-    def identity(sentence: List[str]):
-        return sentence
+    def identity(s: str) -> str:
+        return s

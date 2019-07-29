47e934d41e652eb076c07cb45a2cfa121174bc2e
JojoFisherman <15219666@life.hkbu.edu.hk>
2019-07-30 01:51:39 +0800

    Added more options and filling in the process method

--- data/field.py
+++ data/field.py
@@ -1,31 +1,64 @@
-from typing import Callable, List, Union
+from typing import Callable, List, Union, Tuple
+from .pipeline import Pipeline
+import torch
+
+
+def naive_tokenizer(s: str) -> List[str]:
+    return s.split()
 
 
 class Field:
     def __init__(
         self,
-        preprocessing: List[Callable[List[str], List[str]]] = None,
+        preprocessing: List[Tuple[str, Callable[[str], str]]] = None,
         postprocessing=None,
         is_target=False,
+        tokenizer: Callable[[str], List[str]] = naive_tokenizer,
         lower=False,
-        tokenize: Callable[[str], List[str]] = None,
-        sequential: bool = True,
-        numericalize: bool = True,
+        is_sequential: bool = True,
+        to_numericalize: bool = True,
+        include_lengths: bool = False,
+        batch_first: bool = False,
+        stopwords: List = None,
+        pad_token: str = "<pad>",
+        unk_token: str = "<unk>",
+        sos_token: str = None,
+        eos_token: str = None,
+        pad_first: bool = False,
+        truncate_first: bool = False,
     ):
-        self.preprocessing = preprocessing
+        self.preprocessing = Pipeline(preprocessing)
         self.postprocessing = postprocessing
         self.is_target = is_target
+        self.tokenizer = tokenizer
         self.lower = lower
-        self.tokenize = tokenize
-        if not self.tokenize:
-            tokenize = lambda s: s.split()
-        self.sequential = sequential
-        self.numericalize = numericalize
+        if lower:
+            self.preprocessing = Pipeline([("lower", str.lower)]).add(
+                self.preprocessing
+            )
+        self.is_sequential = is_sequential
+        self.to_numericalize = to_numericalize
+        self.include_lengths = include_lengths
+        self.batch_first = batch_first
+        self.stopwords = set(stopwords)
+        self.pad_token = pad_token
+        self.unk_token = unk_token
+        self.sos_token = sos_token
+        self.eos_token = eos_token
+        self.pad_first = pad_first
+        self.truncate_first = truncate_first
 
     def preprocess(self, s: str) -> Union[str, List[str]]:
-        if self.sequential:
+        if self.s_sequential:
             s = self.tokenize(s)  # List[str]
-        if self.processing:
-            return self.processing(s)
+        if self.preprocessing:
+            return self.preprocessing(s)
         else:
             return s
+
+    def process(self, batch: List[List[str]]) -> torch.Tensor:
+        """Do the padding and the numericalizing to create a torch.Tensor
+        """
+        batch = self.pad(batch)
+        batch = self.numericalize(batch)
+        return torch.tensor(batch, dtype=torch.long)

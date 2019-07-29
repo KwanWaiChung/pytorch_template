e31f16d42de02b4124a498e69dc3f867ec875286
JojoFisherman <15219666@life.hkbu.edu.hk>
2019-07-30 01:52:10 +0800

    Added more pipeline test case

--- test/test_pipeline.py
+++ test/test_pipeline.py
@@ -12,11 +12,17 @@ def get_pipeline():
     return p
 
 
-def test_pipeline_creation():
+def test_pipeline_process_list():
     p = get_pipeline()
     sentence = ["HELLO", "HEllo", "hello"]
     for s in p(sentence):
         assert s == "hel"
+
+
+def test_pipeline_process_str():
+    p = get_pipeline()
+    sentence = "HELLO"
+    assert p(sentence) == "hel"
 
 
 def test_pipeline_add_list():
@@ -29,6 +35,23 @@ def test_pipeline_add_list():
         return s + "_success"
 
     p = p.add(("upper", func1)).add(("add_words", func2))
+    assert p.steps[2].name == "upper"
+    assert p.steps[3].name == "add_words"
+    sentence = ["HELLO", "HEllo", "hello"]
+    for s in p(sentence):
+        assert s == "HEL_success"
+
+
+def test_pipeline_add_pipeine():
+    def func1(s: str) -> str:
+        return s.upper()
+
+    def func2(s: str) -> str:
+        return s + "_success"
+
+    p = get_pipeline()
+    p2 = Pipeline([("upper", func1), ("add_words", func2)])
+    p.add(p2)
     assert p.steps[2].name == "upper"
     assert p.steps[3].name == "add_words"
     sentence = ["HELLO", "HEllo", "hello"]

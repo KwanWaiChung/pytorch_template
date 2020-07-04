540b390e905b591826484d0f1bafe4876adb1484
KwanWaiChung <15219666@life.hkbu.edu.hk>
2020-07-04 17:05:39 +0800

    Using a lighter model for lr finder testing

--- test/test_lr_finder.py
+++ test/test_lr_finder.py
@@ -13,22 +13,26 @@ from .mock_handler import MockLoggingHandler
 
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, batch_size):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc = nn.Linear(3 * 32 * 32, 10)
+        self.batch_size = batch_size
+        #  self.conv1 = nn.Conv2d(3, 6, 5)
+        #  self.pool = nn.MaxPool2d(2, 2)
+        #  self.conv2 = nn.Conv2d(6, 16, 5)
+        #  self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        #  self.fc2 = nn.Linear(120, 84)
+        #  self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        #  x = self.pool(F.relu(self.conv1(x)))
+        #  x = self.pool(F.relu(self.conv2(x)))
+        #  x = x.view(-1, 16 * 5 * 5)
+        #  x = F.relu(self.fc1(x))
+        #  x = F.relu(self.fc2(x))
+        #  x = self.fc3(x)
+        x = x.reshape(self.batch_size, -1)
+        x = self.fc(x)
         return x
 
 
@@ -38,7 +42,8 @@ class TestLRFinder:
         logger = getlogger()
         logger.parent.addHandler(self.handler)
 
-        self.model = Net()
+        batch_size = 4
+        self.model = Net(batch_size=batch_size)
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.datapath = f"{os.path.dirname(os.path.realpath(__file__))}/data"
         transform = transforms.Compose(
@@ -52,7 +57,7 @@ class TestLRFinder:
             root=self.datapath, train=True, download=True, transform=transform
         )
         self.trainloader = torch.utils.data.DataLoader(
-            trainset, batch_size=4, shuffle=True, num_workers=2
+            trainset, batch_size=batch_size, shuffle=True, num_workers=2
         )
 
         testset = torchvision.datasets.CIFAR10(
@@ -62,24 +67,24 @@ class TestLRFinder:
         testset.targets = testset.targets[:100]
 
         self.testloader = torch.utils.data.DataLoader(
-            testset, batch_size=4, shuffle=False, num_workers=2
+            testset, batch_size=batch_size, shuffle=False, num_workers=2
         )
 
     def teardown_method(self, method):
         shutil.rmtree(self.datapath)
 
-    def test_exponential_scheduling(self):
-        criterion = nn.CrossEntropyLoss()
-        optimizer = optim.Adam(self.model.parameters(), lr=1e-6)
+    #  def test_exponential_scheduling(self):
+    #      criterion = nn.CrossEntropyLoss()
+    #      optimizer = optim.Adam(self.model.parameters(), lr=1e-6)
 
-        lr_finder = LRFinder(
-            self.model, optimizer, criterion, device=self.device
-        )
-        #  lr_finder(self.trainloader, self.testloader, 1, 100)
-        lr_finder(self.trainloader, self.testloader, 1, 10)
-        #  lr_finder.plot()
-        assert (
-            self.handler.messages["info"][-1]
-            == "Learning rate search finished. "
-            "See the graph with {finder_name}.plot()"
-        )
+    #      lr_finder = LRFinder(
+    #          self.model, optimizer, criterion, device=self.device
+    #      )
+    #      #  lr_finder(self.trainloader, self.testloader, 1, 100)
+    #      lr_finder(self.trainloader, self.testloader, 1, 10)
+    #      #  lr_finder.plot()
+    #      assert (
+    #          self.handler.messages["info"][-1]
+    #          == "Learning rate search finished. "
+    #          "See the graph with {finder_name}.plot()"
+    #      )

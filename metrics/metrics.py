617c8efa17d94b3f00aedb20cb43b10bd8b3ffe7
KwanWaiChung <15219666@life.hkbu.edu.hk>
2020-08-05 14:49:11 +0800

    correct division error

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -28,7 +28,7 @@ class Metric(Callback):
         self.reset()
 
     def on_train_batch_end(self, logs):
-        logs[self.name] = self(logs["last_y_true"], logs["last_y_pred"]).item()
+        logs[self.name] = self(logs["last_y_true"], logs["last_y_pred"])
 
     def on_val_start(self, logs):
         self.reset()
@@ -36,7 +36,7 @@ class Metric(Callback):
     def on_test_batch_end(self, logs):
         logs["val_" + self.name] = self(
             logs["last_y_true"], logs["last_y_pred"]
-        ).item()
+        )
 
 
 class Accuracy(Metric):
@@ -47,7 +47,7 @@ class Accuracy(Metric):
 
     def __call__(self, y_true, y_pred):
         self.n_samples += y_true.shape[0]
-        self.n_correct += (y_true == y_pred).sum()
+        self.n_correct += (y_true == y_pred).sum().item()
         return self.n_correct / self.n_samples
 
     def reset(self):
